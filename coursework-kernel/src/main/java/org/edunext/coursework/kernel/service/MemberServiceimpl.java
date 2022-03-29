5a13f4461090dc6d553ad5985235d264ebc44f60
jingjianxin <1429242983@qq.com>
2022-03-29 17:45:19 +0800

    [Added] 添加MemberServiceimpl

---
+++ coursework-kernel/src/main/java/org/edunext/coursework/kernel/service/MemberServiceimpl.java
@@ -0,0 +1,13 @@
+package org.edunext.coursework.kernel.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * @author jingjianxin
+ */
+@Service
+public class MemberServiceimpl implements MemberService {
+
+}

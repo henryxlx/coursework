f24c001ff064e0cbe1afa43e1ee65102f66ef8b7
xulixin <henryviolet@sina.com>
2023-06-08 07:56:05 +0100

    feat: 实现全部的课程成员数据访问对象功能CourseMemberDaoImpl

--- coursework-kernel-dao-impl/src/main/java/org/edunext/coursework/kernel/dao/impl/CourseMemberDaoImpl.java
+++ coursework-kernel-dao-impl/src/main/java/org/edunext/coursework/kernel/dao/impl/CourseMemberDaoImpl.java
@@ -1,11 +1,16 @@
 package org.edunext.coursework.kernel.dao.impl;
 
+import com.jetwinner.util.MapUtil;
+import com.jetwinner.util.ValueParser;
 import com.jetwinner.webfast.dao.support.DynamicQueryBuilder;
 import com.jetwinner.webfast.dao.support.FastJdbcDaoSupport;
+import com.jetwinner.webfast.kernel.dao.support.OrderBy;
 import org.edunext.coursework.kernel.dao.CourseMemberDao;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -19,29 +24,44 @@ public class CourseMemberDaoImpl extends FastJdbcDaoSupport implements CourseMem
 
     @Override
     public void batchDeleteMember(List<Object> ids) {
+        String sql = "DELETE FROM " + TABLE_NAME + " WHERE id = ?";
+        getJdbcTemplate().batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setInt(1, ValueParser.parseInt(ids.get(i)));
+            }
 
+            @Override
+            public int getBatchSize() {
+                return ids.size();
+            }
+        });
     }
 
     @Override
     public Map<String, Object> getMemberByCourseIdAndUserId(Integer courseId, Object userId) {
-        return null;
+        String sql = "SELECT * FROM " + TABLE_NAME + " WHERE userId = ? AND courseId = ? LIMIT 1";
+        return getJdbcTemplate().queryForList(sql, userId, courseId).stream().findFirst().orElse(MapUtil.newHashMap(0));
     }
 
     @Override
     public void deleteMember(Object id) {
-
+        String sql = "DELETE FROM " + TABLE_NAME + " WHERE id = ?";
+        getJdbcTemplate().update(sql, id);
     }
 
     @Override
     public void addMember(Map<String, Object> member) {
-
+        int id = insertMapReturnKey(TABLE_NAME, member).intValue();
+        member.put("id", id);
     }
 
     @Override
     public List<Map<String, Object>> findMembersByCourseIdAndRole(Integer courseId, String roleName,
                                                                   Integer start, Integer limit) {
 
-        return new ArrayList<>(0);
+        String sql = "SELECT * FROM " + TABLE_NAME + " WHERE courseId = ? AND role = ? ORDER BY seq,createdTime DESC LIMIT " + start + ", " + limit;
+        return getJdbcTemplate().queryForList(sql, courseId, roleName);
     }
 
     @Override
@@ -52,37 +72,72 @@ public class CourseMemberDaoImpl extends FastJdbcDaoSupport implements CourseMem
 
     @Override
     public List<Map<String, Object>> searchMember(Map<String, Object> conditions, Integer start, Integer limit) {
-        return new ArrayList<>(0);
+        DynamicQueryBuilder builder = this.createSearchQueryBuilder(conditions)
+                .select("*")
+                .setFirstResult(start)
+                .setMaxResults(limit)
+                .orderBy(OrderBy.build(1).addAsc("createdTime"));
+        return getNamedParameterJdbcTemplate().queryForList(builder.getSQL(), conditions);
     }
 
     @Override
     public Integer findMemberCountByUserIdAndRole(Integer userId, String role, boolean onlyPublished) {
-        return 0;
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT COUNT( m.courseId ) FROM ").append(TABLE_NAME).append(" m ");
+        sql.append(" JOIN  ").append(CourseDaoImpl.TABLE_NAME).append(" AS c ON m.userId = ? ");
+        sql.append(" AND m.role =  ? AND m.courseId = c.id ");
+        if (onlyPublished) {
+            sql.append(" AND c.status = 'published' ");
+        }
+        return getJdbcTemplate().queryForObject(sql.toString(), Integer.class, userId, role);
     }
 
     @Override
     public Integer findMemberCountByUserIdAndCourseTypeAndIsLearned(Integer userId, String role, String type, Integer isLearned) {
-        return 0;
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT COUNT( m.courseId ) FROM ").append(TABLE_NAME).append(" m ");
+        sql.append(" JOIN  ").append(CourseDaoImpl.TABLE_NAME).append(" AS c ON m.userId = ? ");
+        sql.append(" AND c.type =  ? AND m.courseId = c.id  AND m.isLearned = ? AND m.role = ?");
+
+        return getJdbcTemplate().queryForObject(sql.toString(), Integer.class, userId, type, isLearned, role);
     }
 
     @Override
     public Integer findMemberCountByUserIdAndRoleAndIsLearned(Integer userId, String role, Integer isLearned) {
-        return 0;
+        String sql = "SELECT COUNT(*) FROM " + TABLE_NAME + " WHERE  userId = ? AND role = ? AND isLearned = ?";
+        return getJdbcTemplate().queryForObject(sql, Integer.class, userId, role, isLearned);
     }
 
     @Override
     public List<Map<String, Object>> findMembersByUserIdAndRole(Integer userId, String role, Integer start, Integer limit, boolean onlyPublished) {
-        return new ArrayList<>(0);
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT m.* FROM ").append(TABLE_NAME).append(" m ");
+        sql.append(" JOIN  ").append(CourseDaoImpl.TABLE_NAME).append(" AS c ON m.userId = ? ");
+        sql.append(" AND m.role =  ? AND m.courseId = c.id ");
+        if (onlyPublished) {
+            sql.append(" AND c.status = 'published' ");
+        }
+        sql.append(" ORDER BY createdTime DESC LIMIT ").append(start).append(", ").append(limit);
+
+        return getJdbcTemplate().queryForList(sql.toString(), userId, role);
     }
 
     @Override
     public List<Map<String, Object>> findMembersByUserIdAndCourseTypeAndIsLearned(Integer userId, String role, String type, String isLearned, Integer start, Integer limit) {
-        return new ArrayList<>(0);
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT m.* FROM ").append(TABLE_NAME).append(" m ");
+        sql.append(" JOIN  ").append(CourseDaoImpl.TABLE_NAME).append(" AS c ON m.userId = ? ");
+        sql.append(" AND c.type =  ? AND m.courseId = c.id AND m.isLearned = ? AND m.role = ?");
+        sql.append(" ORDER BY createdTime DESC LIMIT ").append(start).append(", ").append(limit);
+
+        return getJdbcTemplate().queryForList(sql.toString(), userId, type, isLearned, role);
     }
 
     @Override
     public List<Map<String, Object>> findMembersByUserIdAndRoleAndIsLearned(Integer userId, String role, String isLearned, Integer start, Integer limit) {
-        return new ArrayList<>(0);
+        String sql = "SELECT * FROM " + TABLE_NAME +
+                " WHERE userId = ? AND role = ? AND isLearned = ? ORDER BY createdTime DESC LIMIT " + start + ", " + limit;
+        return getJdbcTemplate().queryForList(sql, userId, role, isLearned);
     }
 
     private DynamicQueryBuilder createSearchQueryBuilder(Map<String, Object> conditions) {

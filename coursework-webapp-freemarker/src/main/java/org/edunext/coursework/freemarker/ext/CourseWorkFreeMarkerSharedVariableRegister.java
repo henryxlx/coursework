e94d43da4c8ffc6d5e9344d7b0a97a4d7057f834
xulixin <henryviolet@sina.com>
2023-06-27 01:13:52 +0100

    feat: 页面定义函数类CourseWorkFreeMarkerSharedVariableRegister添加将ASCII值转换为字符的函数chr

--- coursework-webapp-freemarker/src/main/java/org/edunext/coursework/freemarker/ext/CourseWorkFreeMarkerSharedVariableRegister.java
+++ coursework-webapp-freemarker/src/main/java/org/edunext/coursework/freemarker/ext/CourseWorkFreeMarkerSharedVariableRegister.java
@@ -1,6 +1,7 @@
 package org.edunext.coursework.freemarker.ext;
 
 import com.jetwinner.webfast.freemarker.ext.BaseFreeMarkerSharedVariableRegister;
+import freemarker.template.SimpleNumber;
 import freemarker.template.SimpleScalar;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,8 @@ public class CourseWorkFreeMarkerSharedVariableRegister extends BaseFreeMarkerSh
     public CourseWorkFreeMarkerSharedVariableRegister() {
         this.sharedVariableMap = new HashMap<>(1);
         fillQuestionStemTextFilter();
+        fillQuestionStemHtmlFilter();
+        charFilter();
     }
 
     private void fillQuestionStemTextFilter() {
@@ -27,6 +30,38 @@ public class CourseWorkFreeMarkerSharedVariableRegister extends BaseFreeMarkerSh
                 return stem.replaceAll("\\[\\[.+?\\]\\]", "____");
             }
             return stem;
+        });
+    }
+
+    private void fillQuestionStemHtmlFilter() {
+        String filterName = "fill_question_stem_html";
+        this.sharedVariableMap.put(filterName, args -> {
+            validFilterArguments(args, 1, filterName);
+            SimpleScalar scalar = (SimpleScalar) args.get(0);
+            String stem = scalar.getAsString();
+            if (stem != null && stem.indexOf("[[") >= 0 && stem.indexOf("]]") >= 0) {
+//                $index = 0;
+//                $stem = preg_replace_callback('/\[\[.+?\]\]/', function($matches) use (&$index) {
+//                    $index ++;
+//                    return "<span class='question-stem-fill-blank'>({$index})</span>";
+//                }, $stem);
+//                return $stem;
+            }
+            return stem;
+        });
+    }
+
+    private void charFilter() {
+        String filterName = "chr";
+        this.sharedVariableMap.put(filterName, args -> {
+            validFilterArguments(args, 1, filterName);
+            SimpleNumber number = (SimpleNumber) args.get(0);
+            int asciiCode = number.getAsNumber().intValue();
+            if (asciiCode >= 0 && asciiCode < 256) {
+                char ch = (char) asciiCode;
+                return "" + ch;
+            }
+            return "" + asciiCode;
         });
     }
 }
